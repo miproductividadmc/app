18b8688b1d7ba41dacb1f6c6e45d097a9dda8deb
ezequielpascucciello-jpg <ezequielpascucciello@gmail.com>
2026-09-26 00:53:21 -0300

    Crear AnalisisOperativoV2

---
+++ src/AnalisisOperativoV2.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function AnalisisOperativoV2() {
+
+  return (
+
+    <section className="panel people-analysis">
+
+      <div className="analysis-title">
+
+        <div>
+          <small>ADMINISTRADOR 2.0</small>
+
+          <h2>
+            Centro de Control
+          </h2>
+
+          <p>
+            Prueba funcionando correctamente.
+          </p>
+
+        </div>
+
+      </div>
+
+    </section>
+
+  );
+
+}
